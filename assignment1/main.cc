e07c7f4c8e5047a6d075433bd0c2b7229b31edcf
Soham <113625563+Soham-Chatt@users.noreply.github.com>
2023-09-20 19:52:45 +0200

    Split int main() into multiple parts (getting user input and parsing/evaluating the expression) for improved maintainability and modularity.

--- assignment1/main.cc
+++ assignment1/main.cc
@@ -4,7 +4,7 @@
 #include <string> // std::string
 #include <vector> // For the (multi-line) expressions
 
-
+// Short introduction to the program
 void intro() {
     std::cout << "This program is meant for parsing lambda calculus expressions.\n"
                  "The program will parse the expression and print the abstract syntax tree and the result.\n"
@@ -13,12 +13,8 @@ void intro() {
                  std::endl;
 }
 
-int main() {
-    intro(); // Short introduction to the program
-
-    std::vector<std::string> inputs;
+void getInputs(std::vector<std::string>& inputs) {
     std::string input;
-    int exprNr = 1;
 
     // Get the expressions from the user
     while (true) {
@@ -28,9 +24,10 @@ int main() {
         }
         inputs.push_back(input);
     }
+}
 
-    Parser parser;
-
+void parseAndEvaluateExpressions(const std::vector<std::string>& inputs, Parser& parser) {
+    int exprNr = 1;
     // Iterating through each expression of input and keeping track of the expression number
     for (const auto& inp : inputs) {
         Node* ast = nullptr;
@@ -39,7 +36,7 @@ int main() {
         } catch (const std::runtime_error& e) {
             std::cerr << "Expression " << exprNr << ": " << e.what() << '\n';
             delete ast;
-            exprNr++; // Increment expression number
+            exprNr++;
             continue;
         }
 
@@ -51,9 +48,20 @@ int main() {
         delete ast;
         exprNr++; // Increment expression number
     }
+}
+
+int main() {
+    intro();
+
+    std::vector<std::string> inputs;
+    getInputs(inputs);
+
+    Parser parser;
+    parseAndEvaluateExpressions(inputs, parser);
 
     return 0;
 }
+
 
 
 
