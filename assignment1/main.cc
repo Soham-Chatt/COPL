6726afb786362f43259581010d0ba6eded5f7720
Soham <113625563+Soham-Chatt@users.noreply.github.com>
2023-09-20 19:16:21 +0200

    Added multiline parsing

--- assignment1/main.cc
+++ assignment1/main.cc
@@ -189,28 +189,44 @@ void print_tree(Node* node, int depth = 0) {
 }
 
 int main() {
+    std::cout << "Enter lambda expressions to parse, one per line. Press enter on an empty line to finish.\n";
+    std::vector<std::string> inputs;
     std::string input;
-    std::getline(std::cin, input);
+    int exprNr = 1;
 
-    Parser parser;
-    Node* ast = nullptr;
-
-    // Try to parse the input string and otherwise return 1 (failed)
-    try {
-        ast = parser.parse(input);
-    } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << '\n';
-        delete ast;
-        return 1;
+    // Get the expressions from the user
+    while (true) {
+        std::getline(std::cin, input);
+        if (input.empty()) {
+            break;
+        }
+        inputs.push_back(input);
     }
 
-    // Print the tree
-    print_tree(ast);
+    Parser parser;
 
-    // Print the unambiguous string representation of the AST
-    std::cout << "Result: " << ast->to_string() << '\n';
+    // Iterating through each expression of input and keeping track of the expression number
+    for (const auto& inp : inputs) {
+        Node* ast = nullptr;
+        try {
+            ast = parser.parse(inp);
+        } catch (const std::runtime_error& e) {
+            std::cerr << "Expression " << exprNr << ": " << e.what() << '\n';
+            delete ast;
+            exprNr++; // Increment expression number
+            continue;
+        }
 
-    delete ast;
-    return 0; // Successfully parsed
+        // Displaying the expression number before printing the tree and the result
+        std::cout << "Expression " << exprNr << ":\n";
+        print_tree(ast);
+        std::cout << "Result: " << ast->to_string() << std::endl << std::endl;
+
+        delete ast;
+        exprNr++; // Increment expression number
+    }
+    return 0;
 }
+
+
 
