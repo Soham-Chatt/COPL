93f468502202761c2ff13024d4e43983927b78a4
Soham <113625563+Soham-Chatt@users.noreply.github.com>
2023-10-13 16:59:57 +0200

    Added multi-line support for file input

--- assignment2/main.cc
+++ assignment2/main.cc
@@ -6,7 +6,6 @@
 #include <unordered_set>
 
 int main(int argc, char *argv[]) {
-  // Take file argument
   if (argc != 2) {
     std::cerr << "Usage: " << argv[0] << " <file_name>" << std::endl;
     return 1;
@@ -18,29 +17,25 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  // Read the file
-  std::string line, expression;
-  while (std::getline(inFile, line)) {
-    expression += line;
-  }
-
-
+  std::string line;
   Parser parser;
-  Node* root = parser.parse(expression); // Parse and create AST
-
   Interpreter interpreter;
-  std::unordered_set<std::string> bound_vars;
 
-  int iterations = 0;
-  Node* reduced = interpreter.eval(root, bound_vars, iterations);
+  while (std::getline(inFile, line)) {
+    Node* root = parser.parse(line); // Parse each line and create AST
 
+    std::unordered_set<std::string> bound_vars;
+    int iterations = 0;
 
-  // Output the reduced expression
-  if (reduced) {
-    std::cout << "Reduced expression: " << reduced->to_string() << std::endl;
-  } else {
-    std::cout << "Could not reduce the expression further." << std::endl;
+    Node* reduced = interpreter.eval(root, bound_vars, iterations);
+
+    if (reduced) {
+      std::cout << "Reduced expression: " << reduced->to_string() << std::endl;
+    } else {
+      std::cout << "Could not reduce the expression further." << std::endl;
+    }
   }
 
   return 0;
 }
+
