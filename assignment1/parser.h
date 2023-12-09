c14a26e115a10036c3ad92d279b9895ef3d9f5e4
Soham <113625563+Soham-Chatt@users.noreply.github.com>
2023-12-09 21:30:18 +0100

    Unique pointers for assignment 1

--- assignment1/parser.h
+++ assignment1/parser.h
@@ -1,70 +1,57 @@
-// parser.h
 #ifndef PARSER_H
 #define PARSER_H
 
 #include <string>
 #include <iostream>
-#include <vector>
+#include <memory>
 #include <cctype>
+#include <sstream>
 
 class Node {
 public:
-    virtual std::string to_string() const = 0;
-    int depth;
-
-    Node() {
-        depth = 0;
-    }
-
-    virtual ~Node() = default;
+  virtual ~Node() = default;
+  virtual std::string to_string() const = 0;
 };
 
 class VariableNode : public Node {
 public:
-    std::string name;
-    VariableNode(const std::string& name);
-
-    std::string to_string() const override;
+  std::string name;
+  VariableNode(const std::string& name);
+  std::string to_string() const override;
 };
 
 class LambdaNode : public Node {
 public:
-    std::string param;
-    Node* body;
-    LambdaNode(const std::string& param, Node* body);
-
-    std::string to_string() const override;
-
-    ~LambdaNode();
+  std::string param;
+  std::unique_ptr<Node> body;
+  LambdaNode(const std::string& param, std::unique_ptr<Node> body);
+  std::string to_string() const override;
 };
 
 class ApplicationNode : public Node {
 public:
-    Node* left;
-    Node* right;
-    ApplicationNode(Node* left, Node* right);
-
-    std::string to_string() const override;
-
-    ~ApplicationNode();
+  std::unique_ptr<Node> left;
+  std::unique_ptr<Node> right;
+  ApplicationNode(std::unique_ptr<Node> left, std::unique_ptr<Node> right);
+  std::string to_string() const override;
 };
 
 class Parser {
 public:
-    Node* parse(const std::string& input_str);
+  std::unique_ptr<Node> parse(const std::string& input_str);
 private:
-    std::string input;
-    size_t pos = 0;
+  std::string input;
+  size_t pos = 0;
 
-    char current_char();
-    void skip_whitespace();
-    std::string parse_variable();
-    Node* parse_expression();
-    Node* parse_atom();
-    Node* parse_lambda();
+  char current_char();
+  void skip_whitespace();
+  std::string parse_variable();
+  std::unique_ptr<Node> parse_expression();
+  std::unique_ptr<Node> parse_atom();
+  std::unique_ptr<Node> parse_lambda();
 };
 
-void print_tree(Node* node);
-void assign_depth(Node* node, int depth = 0);
-std::string generate_dot(Node* node);
-#endif //PARSER_H
+void print_tree(const Node* node, int depth = 0);
+std::string generate_dot(const Node* node);
+
+#endif // PARSER_H
